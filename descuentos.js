967940fb1a986809717a2f22f722415a7d25d1ab
Matias Ruiz <matiasrm14@gmail.com>
2021-12-02 11:51:50 -0300

    Hicimos las funciones para que el usuario ingrese el precio y el cupon y le calcule el descuento

---
+++ descuentos.js
@@ -0,0 +1,48 @@
+const coupons = [
+    {
+        name: "cupon1",
+        discount: 15,
+    },
+    {
+        name: "cupon2",
+        discount: 30,
+    },
+    {
+        name: "cupon3",
+        discount: 55,
+    },
+];
+
+
+function calcularPrecioConDescuento(precio, descuento){
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+    return precioConDescuento;
+}
+
+function onClickButtonPriceDiscount(){
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
+
+    const isCouponValueValid = function (coupon) {
+        return coupon.name === couponValue;
+    };
+
+    const userCoupon = coupons.find(isCouponValueValid);
+
+    if (!userCoupon) {
+        alert("El cupón " + couponValue + " no es válido");
+    } else {
+        const descuento = userCoupon.discount;
+        const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+
+        const resultP = document.getElementById("ResultPrice");
+        resultP.innerText = "El precio con descuento es: $" + precioConDescuento;
+    }
+
+}
+
